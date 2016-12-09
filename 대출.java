d3e1dfc554ff31cd3b3fc9ec4b494c9aa5ae797a
wool901 <sc3321@naver.com>
2016-12-10 02:17:24 +0900

    대출 및 회원 class 작성

---
+++ 대출.java
@@ -0,0 +1,41 @@
+
+public class 대출 {
+private 	int 대출코드;
+private int 연체;
+private int 대출권수;
+private int 대출가격;
+private int 대출기간;
+
+
+public int get대출코드() {
+	return 대출코드;
+}
+public void set대출코드(int 대출코드) {
+	this.대출코드 = 대출코드;
+}
+public int get연체() {
+	return 연체;
+}
+public void set연체(int 연체) {
+	this.연체 = 연체;
+}
+public int get대출권수() {
+	return 대출권수;
+}
+public void set대출권수(int 대출권수) {
+	this.대출권수 = 대출권수;
+}
+public int get대출가격() {
+	return 대출가격;
+}
+public void set대출가격(int 대출가격) {
+	this.대출가격 = 대출가격;
+}
+public int get대출기간() {
+	return 대출기간;
+}
+public void set대출기간(int 대출기간) {
+	this.대출기간 = 대출기간;
+}
+}
+
